40f08a21e960bbc73f87bba9a697441daf06c1e8
Vinícius Krolow <krolow@gmail.com>
2015-01-05 08:25:03 -0200

    Initial vagrant

---
+++ Vagrantfile
@@ -0,0 +1,36 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+def install_dep(name, params = nil)
+  "(mkdir -p /etc/puppet/modules && puppet module --modulepath /etc/puppet/modules list | grep #{name}) || puppet module install #{name} #{params}"
+end
+
+Vagrant.configure("2") do |config|
+  # All Vagrant configuration is done here. The most common configuration
+  # options are documented and commented below. For a complete reference,
+  # please see the online documentation at vagrantup.com.
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "puphpet/debian75-x32"
+
+
+  config.vm.provision :shell do |shell|
+    commands = []
+    commands << "if [[ ! -f /apt-get-run ]]; then sudo apt-get update && sudo apt-get upgrade && sudo touch /apt-get-run; fi"    
+    commands << install_dep("maestrodev-rvm", "-v 1.8.1")
+    commands << install_dep("puppetlabs-postgresql", "-v 4.1.0")
+    commands << install_dep("willdurand-nodejs", "-v 1.8.4")
+    commands << install_dep("maestrodev-android", "-v 1.2.0")
+    commands << install_dep("puppetlabs-java", "-v 1.2.0")
+
+    shell.inline = commands.join(';')
+  end
+
+  config.vm.provision "puppet" do |puppet|
+    puppet.manifests_path = "manifests"
+    puppet.options = ['--verbose']
+  end
+
+  config.vm.synced_folder "../", "/srv/stalker/"
+
+end
---
+++ manifests/default.pp
@@ -0,0 +1,84 @@
+package { ['usbutils', 'git', 'vim-nox']:
+  ensure => 'installed'
+}
+
+class { 'nodejs':
+  version => "v0.10.35"
+}
+
+package { 'cordova':
+  provider => 'npm',
+  require  => Class['nodejs']
+}
+
+package { 'ionic':
+  provider => 'npm',
+  require  => Class['nodejs']
+}
+
+package { 'bower':
+  provider => 'npm',
+  require  => Class['nodejs']
+}
+
+class { 'java':
+  distribution => 'jdk',
+  version      => 'latest',
+} ->
+class { 'android': }
+android::platform { 'android-10': }
+android::platform { 'android-16': }
+android::platform { 'android-18': }
+
+file { '/bin/adb':
+   ensure => 'link',
+   target => '/home/vagrant/android/android-sdk-linux/platform-tools/adb',
+   require => [Class['android']]
+}
+
+file { '/bin/android':
+   ensure => 'link',
+   target => '/home/vagrant/android/android-sdk-linux/tools/android',
+   require => [Class['android']]
+}
+
+file { "/home/vagrant/android/android-sdk-linux/platform-tools/adb":
+  owner => root,
+  group => vagrant,
+  mode => 4550,
+  require => [Class['android']]
+}
+
+
+class { 'postgresql::server': }
+
+postgresql::server::db { 'project':
+  user     => 'project',
+  password => postgresql_password('project', 'project'),
+}
+
+include rvm
+
+rvm_system_ruby {
+  'ruby-2.2.0':
+    ensure      => 'present',
+    default_use => true
+}
+
+rvm_gem {
+  'rails-api':
+    name         => 'rails-api',
+    ruby_version => 'ruby-2.2.0',
+    ensure       => latest,
+    require      => Rvm_system_ruby['ruby-2.2.0'];
+  'rails':
+    name         => 'rails',
+    ruby_version => 'ruby-2.2.0',
+    ensure       => "4.2.0",
+    require      => Rvm_system_ruby['ruby-2.2.0'];
+  'puppet':
+    name         => 'puppet',
+    ruby_version => 'ruby-2.2.0',
+    ensure       => latest,
+    require      => Rvm_system_ruby['ruby-2.2.0'];
+}
